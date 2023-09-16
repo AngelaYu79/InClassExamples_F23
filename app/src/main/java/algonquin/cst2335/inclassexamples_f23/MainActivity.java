654e16496cb4b85b3cfe82de0fcbdf167b1155e4
Jiajia <yu000251@algonquinlive.com>
2023-09-16 15:00:52 -0400

    added EditText, Button, and TextView

--- app/src/main/java/algonquin/cst2335/inclassexamples_f23/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/inclassexamples_f23/MainActivity.java
@@ -3,6 +3,10 @@ package algonquin.cst2335.inclassexamples_f23;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -15,5 +19,25 @@ public class MainActivity extends AppCompatActivity {
 
         //this is the only function call, loads stuff onto screen
         setContentView(R.layout.activity_main);
+
+        TextView tv = findViewById(R.id.myTextView);
+
+        Button b = findViewById(R.id.myButton);
+
+        EditText et = findViewById(R.id.myEditText);
+
+        b.setOnClickListener( new View.OnClickListener(){
+
+            //(v) -> {//-> makes ir a lambda function \
+            //only run when you click the button
+
+            @Override
+            public void onClick(View v){
+                 tv.setText("You clicked the button");
+                 et.setText("You clicked the button");
+                 b.setText("You clicked the button");
+            }
+
+        });
     }
 }
